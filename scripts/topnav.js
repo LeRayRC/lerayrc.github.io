f31d35e1e73a0d144cb73e69449c87c4c06ac3a6
Carlos Garcia <cgroig@minsait.com>
2023-04-18 19:03:33 +0200

    feat: topnav menu for tablet and mobile

---
+++ scripts/topnav.js
@@ -0,0 +1,9 @@
+
+function verticalMenu() {
+  var x = document.getElementById("myLinks");
+  if (x.style.display === "block") {
+    x.style.display = "none";
+  } else {
+    x.style.display = "block";
+  }
+}
