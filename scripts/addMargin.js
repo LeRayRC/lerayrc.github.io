dd8bb1d8daf9d377a594ce90cdba305df2e6454c
Carlos Garcia <cgroig@minsait.com>
2023-04-19 19:46:25 +0200

    feat: add margin to abilities link

--- scripts/addMargin.js
+++ scripts/addMargin.js
@@ -8,11 +8,15 @@ window.onscroll = function(){
     var scrolledTop = document.documentElement.scrollTop;
     var personaje = document.getElementById("personajeId");
     var enemigo = document.getElementById("enemigosId");
+    var habilidades = document.getElementById("habilidadesId");
     if(scrolledTop - personaje.offsetTop > 100){
         personaje.style.marginTop = "0px"
     }
     if(scrolledTop - enemigo.offsetTop > 100){
         enemigo.style.marginTop = "0px"
+    }
+    if(scrolledTop - habilidades.offsetTop > 100){
+        habilidades.style.marginTop = "0px"
     }
     
 }
