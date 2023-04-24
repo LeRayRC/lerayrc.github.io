e40489c53f297ea61964c2f7c00365c9d68be2b0
Carlos Garcia <cgroig@minsait.com>
2023-04-25 00:20:30 +0200

    feat: add policy privacy overlay

---
+++ scripts/overlay_privacy.js
@@ -0,0 +1,13 @@
+function showPrivacy() {
+    var overlay = document.getElementById("overlay");
+    var popup = document.getElementById("popup");
+    overlay.style.display = "flex";
+    popup.style.display = "flex";
+  }
+  
+  function closePrivacy() {
+    var overlay = document.getElementById("overlay");
+    var popup = document.getElementById("popup");
+    overlay.style.display = "none";
+    popup.style.display = "none";
+  }
