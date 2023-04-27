b07aa84b81f31a0079892500feeebf8a4b05b281
Carlos Garcia <charliroiggarcia12@gmail.com>
2023-04-27 10:33:37 +0200

    fix: margin on links

--- scripts/addMargin.js
+++ scripts/addMargin.js
@@ -4,22 +4,22 @@ function addMargin(divName){
 }
 
 
-window.onscroll = function(){
-    var scrolledTop = document.documentElement.scrollTop;
-    var personaje = document.getElementById("personajeId");
-    var enemigo = document.getElementById("enemigosId");
-    var habilidades = document.getElementById("habilidadesId");
-    var aliados = document.getElementById("aliadosId");
-    if(scrolledTop - personaje.offsetTop > 100){
-        personaje.style.marginTop = "0px"
-    }
-    if(scrolledTop - enemigo.offsetTop > 100){
-        enemigo.style.marginTop = "0px"
-    }
-    if(scrolledTop - habilidades.offsetTop > 100){
-        habilidades.style.marginTop = "0px"
-    }
-    if(scrolledTop - aliados.offsetTop > 100){
-        aliados.style.marginTop = "0px"
-    }
-}
+// window.onscroll = function(){
+//     var scrolledTop = document.documentElement.scrollTop;
+//     var personaje = document.getElementById("personajeId");
+//     var enemigo = document.getElementById("enemigosId");
+//     var habilidades = document.getElementById("habilidadesId");
+//     var aliados = document.getElementById("aliadosId");
+//     if(scrolledTop - personaje.offsetTop > 60){
+//         personaje.style.marginTop = "0px"
+//     }
+//     if(scrolledTop - enemigo.offsetTop > 60){
+//         enemigo.style.marginTop = "0px"
+//     }
+//     if(scrolledTop - habilidades.offsetTop > 60){
+//         habilidades.style.marginTop = "0px"
+//     }
+//     if(scrolledTop - aliados.offsetTop > 60){
+//         aliados.style.marginTop = "0px"
+//     }
+// }
